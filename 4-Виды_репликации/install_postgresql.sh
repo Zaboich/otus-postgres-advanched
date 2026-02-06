9c721b469be2fb4958ee27bc4b6dc2c944b236d1
zaboich <zaboich@gmail.com>
2026-02-06 14:07:09 +0300

    Скрипты инициализации тестов

---
+++ 4-Виды_репликации/install_postgresql.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+export DEBIAN_FRONTEND=noninteractive
+export LC_ALL="ru_RU.UTF-8,"
+export LANGUAGE="ru_RU.UTF-8"
+export LANG="ru_RU.UTF-8"
+export LC_TIME="ru_RU.UTF-8",
+export LC_MONETARY="ru_RU.UTF-8",
+export LC_ADDRESS="ru_RU.UTF-8",
+export LC_TELEPHONE="ru_RU.UTF-8",
+export LC_NAME="ru_RU.UTF-8",
+export LC_MEASUREMENT="ru_RU.UTF-8",
+export LC_IDENTIFICATION="ru_RU.UTF-8",
+export LC_NUMERIC="ru_RU.UTF-8",
+export LC_PAPER="ru_RU.UTF-8",
+
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt update
+sudo apt -y install postgresql mc
--- 4-Виды_репликации/prepare.sh
+++ 4-Виды_репликации/prepare.sh
@@ -1,35 +1,57 @@
 #!/bin/bash
 set -e
-if ! yc vpc network show --name net-otus1; then
+
+# указать расположение публичной части ключа ssh
+SSH_KEY=~/.ssh/id_rsa.pub
+
+
+if ! yc vpc network show --name net-otus1 2>/dev/null; then
   yc vpc network create --name net-otus1;
+  echo "Создана network net-otus1"
+else
+  echo "Работает network net-otus1"
 fi
-echo "Создана network"
 
-if ! yc vpc subnet show --name subnet-otus1; then
+
+if ! yc vpc subnet show --name subnet-otus1 2>/dev/null; then
   yc vpc subnet create --name subnet-otus1 --range 192.168.0.0/24 --network-name net-otus1;
+  echo "Создана subnet subnet-otus1"
+else
+  echo "Работает subnet subnet-otus1"
 fi
-echo "Создана subnet"
 
-yc compute instance create --name vm-otus1 --hostname vm-otus1 --cores 2 --memory 4 --create-boot-disk size=15G,type=network-hdd,image-folder-id=standard-images,image-family=ubuntu-2404-lts --network-interface subnet-name=subnet-otus1,nat-ip-version=ipv4 --ssh-key ~/.ssh/id_rsa.pub
-vm_ip_address1=$(yc compute instance show --name vm-otus1 | grep -E ' +address' | tail -n 1 | awk '{print $2}')
-echo "Создана vm-otus1"
 
-yc compute instance create --name vm-otus2 --hostname vm-otus2 --cores 2 --memory 4 --create-boot-disk size=15G,type=network-hdd,image-folder-id=standard-images,image-family=ubuntu-2404-lts --network-interface subnet-name=subnet-otus1,nat-ip-version=ipv4 --ssh-key ~/.ssh/id_rsa.pub
-vm_ip_address2=$(yc compute instance show --name vm-otus2 | grep -E ' +address' | tail -n 1 | awk '{print $2}')
-echo "Создана vm-otus2"
+if ! yc compute instance show --name  vm-otus1 2>/dev/null; then
+  yc compute instance create --name vm-otus1 --hostname vm-otus1 --cores 2 --memory 4 --create-boot-disk size=15G,type=network-hdd,image-folder-id=standard-images,image-family=ubuntu-2404-lts --network-interface subnet-name=subnet-otus1,nat-ip-version=ipv4 --ssh-key $SSH_KEY
+  echo "Создана vm-otus1"
+else
+  echo "Работает vm-otus1"
+fi
 
-yc compute instance create --name vm-otus3 --hostname vm-otus3 --cores 2 --memory 4 --create-boot-disk size=15G,type=network-hdd,image-folder-id=standard-images,image-family=ubuntu-2404-lts --network-interface subnet-name=subnet-otus1,nat-ip-version=ipv4 --ssh-key ~/.ssh/id_rsa.pub
-vm_ip_address3=$(yc compute instance show --name vm-otus3 | grep -E ' +address' | tail -n 1 | awk '{print $2}')
-echo "Создана vm-otus3"
+if ! yc compute instance show --name  vm-otus2 2>/dev/null; then
+  yc compute instance create --name vm-otus2 --hostname vm-otus2 --cores 2 --memory 4 --create-boot-disk size=15G,type=network-hdd,image-folder-id=standard-images,image-family=ubuntu-2404-lts --network-interface subnet-name=subnet-otus1,nat-ip-version=ipv4 --ssh-key $SSH_KEY
+  echo "Создана vm-otus2"
+else
+  echo "Работает vm-otus2"
+fi
 
-sleep 15
+if ! yc compute instance show --name  vm-otus3 2>/dev/null; then
+  yc compute instance create --name vm-otus3 --hostname vm-otus3 --cores 2 --memory 4 --create-boot-disk size=15G,type=network-hdd,image-folder-id=standard-images,image-family=ubuntu-2404-lts --network-interface subnet-name=subnet-otus1,nat-ip-version=ipv4 --ssh-key $SSH_KEY
+  echo "Создана vm-otus3"
+else
+  echo "Работает vm-otus3"
+fi
 
 echo "Установка Posgresql vm-otus1"
-ssh -o StrictHostKeyChecking=no yc-user@$vm_ip_address1 'bash -s ' < /home/andrey/www/postgres/postgres-otus/yandex_cloud/install_postgresql.sh
+IP_VM1=$(yc compute instance show --name vm-otus1 | grep -E ' +address' | tail -n 1 | awk '{print $2}')
+export IP_VM1
+ssh -o StrictHostKeyChecking=no -t -i $SSH_KEY yc-user@$IP_VM1 'bash -s ' < ./install_postgresql.sh
 echo "Подготовлена vm-otus1"
 
-ssh -o StrictHostKeyChecking=no yc-user@$vm_ip_address2  'bash -s ' < /home/andrey/www/postgres/postgres-otus/yandex_cloud/install_postgresql.sh
+IP_VM2=$(yc compute instance show --name vm-otus2 | grep -E ' +address' | tail -n 1 | awk '{print $2}')
+ssh -o StrictHostKeyChecking=no -t -i $SSH_KEY yc-user@$IP_VM2  'bash -s ' < ./install_postgresql.sh
 echo "Подготовлена vm-otus2"
 
-ssh -o StrictHostKeyChecking=no yc-user@$vm_ip_address3 'bash -s ' < /home/andrey/www/postgres/postgres-otus/yandex_cloud/install_postgresql.sh
+IP_VM3=$(yc compute instance show --name vm-otus3 | grep -E ' +address' | tail -n 1 | awk '{print $2}')
+ssh -o StrictHostKeyChecking=no -t -i $SSH_KEY yc-user@$IP_VM3 'bash -s ' < ./install_postgresql.sh
 echo "Подготовлена vm-otus3"
