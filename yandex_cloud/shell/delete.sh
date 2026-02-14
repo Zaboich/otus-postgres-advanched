ae57bbc624370f024a05dab5a3f9b1886c8d6e04
zaboich <zaboich@gmail.com>
2026-02-14 22:12:11 +0300

    delete dns zone

--- yandex_cloud/shell/delete.sh
+++ yandex_cloud/shell/delete.sh
@@ -5,6 +5,8 @@ for VM_NAME in $(yc compute instance list --format=json | jq -r '.[].name'); do
 done
 #yc compute instance list --format json | jq -r '.[].id' | xargs -I {} echo {} && yc compute instance delete {}
 
+for DNS_NAME in $(yc dns zone list --format=json | jq -r '.[].name' | grep otus | sort -r); do yc dns zone delete $DNS_NAME; done
+
 for SUBNET_NAME in $(yc vpc subnet list --format=json | jq -r '.[].name' | grep otus); do yc vpc subnet delete $SUBNET_NAME; done
 
 for NET_NAME in $(yc vpc network list --format=json | jq -r '.[].name' | grep otus | sort -r); do yc vpc network delete $NET_NAME; done
