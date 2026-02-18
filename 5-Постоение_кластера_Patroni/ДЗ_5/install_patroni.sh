4c52872f7aace76bf281a03007881e436ed6d6b0
zaboich <zaboich@gmail.com>
2026-02-18 23:44:23 +0300

    Развёртывание Patroni 1

--- 5-Постоение_кластера_Patroni/ДЗ_5/install_patroni.sh
+++ 5-Постоение_кластера_Patroni/ДЗ_5/install_patroni.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 set -e
 
-sudo apt install -y -qq python3-venv python-is-python3 >> /dev/null
+# общая часть имён хостов
+HOSTNAME=$(hostname)
+NUM=${HOSTNAME: -1}
+IP_ADDR=$(hostname -I | sed -e 's/[[:space:]]*$//')
 
-sudo mkdir -p /opt/patroni
+
+IP_ADDR1=$(getent hosts vm-otus1 | awk '{ print $1 }')
+IP_ADDR2=$(getent hosts vm-otus2 | awk '{ print $1 }')
+IP_ADDR3=$(getent hosts vm-otus3 | awk '{ print $1 }')
+IP_ADDR4=$(getent hosts vm-otus4 | awk '{ print $1 }')
+IP_ADDR5=$(getent hosts vm-otus5 | awk '{ print $1 }')
+IP_ADDR6=$(getent hosts vm-otus6 | awk '{ print $1 }')
+
+sudo apt install -y -qq python3-venv python-is-python3
+
+sudo mkdir -p -m 755 yc-user /opt/patroni
 sudo mkdir -p /etc/patroni
 sudo mkdir -p /var/log/patroni
 
@@ -18,11 +31,20 @@ deactivate
 
 chown -R postgres:postgres /opt/patroni
 
-# общая часть имён хостов
-HOSTNAME=$(hostname)
-NUM=${HOSTNAME: -1}
-IP_ADDR=$(hostname -I | sed -e 's/[[:space:]]*$//')
+if [ !-f /tmp/patroni.yml ]; then
+  echo "Файл /tmp/patroni.yml не найден. Выход";
+  exit 1;
+fi
 
-IP_ADDR1=$(getent hosts vm-otus1 | awk '{ print $1 }')
-IP_ADDR2=$(getent hosts vm-otus2 | awk '{ print $1 }')
-IP_ADDR3=$(getent hosts vm-otus3 | awk '{ print $1 }')
+sudo mv /tmp/patroni.yml /etc/patroni.yml
+echo "Обработка шаблона /etc/patroni.yml";
+
+# Установка полей в файле шаблона
+sudo sed -i 's/\{HOSTNAME\}/$HOSTNAME/g' /etc/patroni.yml
+sudo sed -i 's/\{IP_ADDR\}/$IP_ADDR/g' /etc/patroni.yml
+sudo sed -i 's/\{HOSTNAME_ETCD1\}/vm-otus1/g' /etc/patroni.yml
+sudo sed -i 's/\{HOSTNAME_ETCD2\}/vm-otus2/g' /etc/patroni.yml
+sudo sed -i 's/\{HOSTNAME_ETCD3\}/vm-otus3/g' /etc/patroni.yml
+
+
+
--- 5-Постоение_кластера_Patroni/ДЗ_5/prepare.sh
+++ 5-Постоение_кластера_Patroni/ДЗ_5/prepare.sh
@@ -3,16 +3,13 @@ set -e
 SSH_KEY=~/.ssh/id_rsa.pub
 NAMESPACE='otus'
 
-QUANTITY=$1
-if [ -z "$QUANTITY" ]; then
-  read -p "Количество vm: " QUANTITY
-fi
-QUANTITY=${QUANTITY:-1}
+# 3 VM для кластера Patroni+Postgres 3 VM для кластера etcd
+QUANTITY=6
 
 QUANT_ETCD=$(($QUANTITY/2))
 QUANT_POSTGRES=$(($QUANTITY/2))
 
-read -p "Установить Postgres? Y/N [N]: " IS_INSTALL
+read -p "Установить ETCD и Postgres? Y/N [N]: " IS_INSTALL
 
 NET="net-${NAMESPACE}"
 SUBNET="subnet-${NAMESPACE}"
@@ -53,21 +50,52 @@ for NUM in $(seq 1 1 $QUANTITY); do
   fi
   IP_VM=$(yc compute instance show --name ${VM_NAME} | grep -E ' +address' | tail -n 1 | awk '{print $2}')
   ADDR_VM[$NUM]=${IP_VM}
+  echo "------------------------------------------------"
 done
-
+# Массив ip созданных VM
 export ADDR_VM
-
+# Устанавливаем прогаммы
 if [[ "$IS_INSTALL" == "Y" || "$IS_INSTALL" == "y" ]]; then
-  for NUM in $(seq 1 1 $QUANT_ETCD); do
-    echo "Установка ETCD vm-${NAMESPACE}${NUM} ";
-    ssh -o StrictHostKeyChecking=no yc-user@${ADDR_VM[$NUM]} 'bash -s ' < ./install_etcd.sh;
-    echo "Подготовлен ETCD ${VM_NAME}";
-  done;
+
+#  for NUM in $(seq 1 1 $QUANT_ETCD); do
+#    VM_NAME="vm-${NAMESPACE}${NUM}"
+#    echo "${VM_NAME} Установка ETCD ";
+#    ssh -o StrictHostKeyChecking=no yc-user@${ADDR_VM[$NUM]} 'bash -s ' < ./install_etcd.sh;
+#    echo "${VM_NAME} Установлен и подготовлен ETCD. Сервис etcd остановлен";
+#    echo "------------------------------------------------"
+#  done;
+#
+#  for NUM in $(seq 1 1 $QUANT_ETCD); do
+#    VM_NAME="vm-${NAMESPACE}${NUM}"
+#    echo "${VM_NAME} Запуск ETCD ";
+#    # Вызывается команда старта сервиса без ожидания результата, чтобы стартовать etcd на всех серверах кластера
+#    ssh -o StrictHostKeyChecking=no yc-user@${ADDR_VM[$NUM]} 'nohup sudo systemctl start etcd  > /dev/null 2>&1 &';
+#    echo "${VM_NAME} Запущен ETCD";
+#  done;
+#
+#  sleep 15;
+#
+#  # Проверка состояния кластера
+#  for NUM in $(seq 1 1 $QUANT_ETCD); do
+#    echo "${VM_NAME} Состояние ETCD";
+#    ssh -o StrictHostKeyChecking=no yc-user@${ADDR_VM[1]} 'etcdctl endpoint status --cluster -w table';
+#  done;
+
 
   for NUM in $(seq $(($QUANT_ETCD + 1)) 1 $QUANTITY); do
+    VM_NAME="vm-${NAMESPACE}${NUM}"
     echo "Установка Posgresql vm-${NAMESPACE}${NUM} ";
     ssh -o StrictHostKeyChecking=no yc-user@${ADDR_VM[$NUM]} 'bash -s ' < ./install_postgresql.sh;
-    echo "Подготовлен POSTGRES ${VM_NAME}";
+    echo "${VM_NAME} Установлен и подготовлен Postgres. Сервис postgres остановлен";
+
+    scp ./patroni.yml yc-user@${ADDR_VM[$NUM]}:/tmp/patroni.yml
+    echo "${VM_NAME} Загружен шаблон конфигурационного файла patroni";
+    scp ./patroni.service yc-user@${ADDR_VM[$NUM]}:/tmp/patroni.service
+    ssh -o StrictHostKeyChecking=no yc-user@${ADDR_VM[$NUM]} 'sudo mv /tmp/patroni.service /etc/systemd/system/patroni.service';
+    echo "${VM_NAME} Загружен файл службы patroni.service";
+
+    ssh -o StrictHostKeyChecking=no yc-user@${ADDR_VM[$NUM]} 'bash -s ' < ./install_patroni.sh;
+    echo "${VM_NAME} Установлен и подготовлен Patroni . Сервис patroni остановлен";
   done;
 fi
 
